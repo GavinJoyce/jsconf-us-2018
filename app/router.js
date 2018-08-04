7bad2c77361ae968ebaf2e092c6b35b0f32918b1
Gavin Joyce <gavinjoyce@gmail.com>
2018-08-04 15:32:41 +0100

    add latency video

--- app/router.js
+++ app/router.js
@@ -20,14 +20,33 @@ Router.map(function() {
     this.role('audience');
 
     this.route('slides', function() {
+      //title
       this.slide('01-title', { transition: 'slide' });
+
+      //intro
       this.slide('02a-intro', { transition: 'slide' });
+
+      //tech
       this.slide('03a-tech', { transition: 'slide' });
+
+      //ember-present
       this.slide('04a-ember-present', { transition: 'slide' });
       this.slide('04z-pointer', { transition: 'slide' });
+
+      //login
       this.slide('05-login', { transition: 'slide' });
+
+      //realtime sounds
       this.slide('06a-realtime-drums', { transition: 'slide' });
+      this.slide('06b-latency-graph', { transition: 'slide' });
+      this.slide('06c-latency-video', { transition: 'slide' });
+      this.slide('06d-clock-drift-graph', { transition: 'slide' });
+      this.slide('06e-synchronised-drums', { transition: 'slide' });
+
+      //beethoven's 5th
       this.slide('07a-beethovens-5th', { transition: 'slide' });
+
+      //thanks
       this.slide('08-thanks', { transition: 'slide' });
     });
   });
