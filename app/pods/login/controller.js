1136e24466f69f25a81b5b64487f7880a93d46c7
Gavin Joyce <gavinjoyce@gmail.com>
2018-08-19 06:05:28 -0700

    style login page

--- app/pods/login/controller.js
+++ app/pods/login/controller.js
@@ -1,6 +1,27 @@
 import Controller from '@ember/controller';
 import { inject } from '@ember/service';
+import { computed } from '@ember/object';
 
 export default Controller.extend({
   session: inject(),
+
+  username: '',
+  password: '',
+
+  revealOptionsCount: 0,
+  showOptions: computed.gte('revealOptionsCount', 5),
+
+  actions: {
+    register() {
+      let username = this.get('username');
+      let password = this.get('password');
+      this.set('showNameIsTaken', false);
+
+      if(username.length === 0) {
+        this.incrementProperty('revealOptionsCount');
+      } else {
+        this.get('session.loginTask').perform(username, password);
+      }
+    }
+  }
 });
