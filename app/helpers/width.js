e9a6451e5edf7885f89bf7dc983a3ac93715dfe6
Gavin Joyce <gavinjoyce@gmail.com>
2018-08-21 09:50:51 -0700

    use ember-present-ember-concurrency slides

--- app/helpers/width.js
+++
@@ -1,7 +0,0 @@
-import { helper } from '@ember/component/helper';
-
-export function computeWidth([start, end, upper]/*, hash*/) {
-  return (end === Infinity) ? upper - start : end - start;
-}
-
-export default helper(computeWidth);
